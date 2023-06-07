eb99212723efbe98067be6e737512373de36e03c
Okiki Ojo <okikio.dev@gmail.com>
2023-06-07 07:16:20 +0000

    chore: ...

--- esbuild.ts
+++ esbuild.ts
@@ -5,12 +5,13 @@ import { join, extname, dirname } from "node:path";
 import { resolve, exports, legacy, imports } from "resolve.exports";
 import { parse as parsePackageName } from "parse-package-name";
 
-import { getCDNUrl, isBareImport, getCDNStyle, inferLoader } from "./utils.js";
+import { getCDNUrl, isBareImport, inferLoader } from "./utils.js";
 import { determineExtension, getRequest } from "./fetch-and-cache.js";
 
 const dir = dirname(fileURLToPath(import.meta.url));
 
 const config: esbuild.BuildOptions = {
+  entryPoints: [join(dir, "./src/index.ts")],
   target: ["esnext"],
   format: "esm",
   bundle: true,
@@ -42,7 +43,6 @@ await esbuild.initialize({});
 console.group("esbuild-wasm - no plugins")
 await esbuild.build({
   ...config,
-  entryPoints: [join(dir, "./src/index.ts")],
   outfile: "dist/no-plugin-bundle.js",
 });
 console.groupEnd()
@@ -53,12 +53,10 @@ const FAILED_PKGJSON_URLs = new Set<string>()
 
 const FileSystem = new Map<string, string>()
 const decoder = new TextDecoder()
-const encoder = new TextEncoder()
 
 console.group("esbuild-wasm - w/ plugins")
 await esbuild.build({
   ...config,
-  entryPoints: [join(dir, "./src/index.ts")],
   outfile: "dist/with-plugin-bundle.js",
   plugins: [
     {
@@ -67,28 +65,30 @@ await esbuild.build({
         build.onResolve({ filter: /.*/ }, async (args) => {
           // Support a different default CDN + allow for custom CDN url schemes
           const { path: argPath, origin } = getCDNUrl(args.path);
+          const path = isBareImport(args.path) ? argPath : join(args.resolveDir, argPath)
+          if (RESOLVED_URLs.has(path)) {
+            return { 
+              path: RESOLVED_URLs.get(argPath), 
+              namespace: 'cdn', 
+              pluginData: args.pluginData
+            }
+          }
   
           if (isBareImport(args.path)) {
-            if (RESOLVED_URLs.has(argPath)) return { path: RESOLVED_URLs.get(argPath), namespace: 'cdn' }
-  
-            // npm standard CDNs, e.g. unpkg, skypack, esm.sh, etc...
-            const NPM_CDN = getCDNStyle(origin) == "npm";
-  
             // Heavily based off of https://github.com/egoist/play-esbuild/blob/main/src/lib/esbuild.ts
             const parsed = parsePackageName(argPath);
             let subpath = parsed.path;
   
-            let pkg = args.pluginData?.pkg ?? { };
-            let oldPkg = pkg;
+            let oldPkg = args.pluginData?.pkg ?? { };
   
             // Are there an dependecies???? Well Goood.
-            const depsExists = "dependencies" in pkg || "devDependencies" in pkg || "peerDependencies" in pkg;
+            const depsExists = "dependencies" in oldPkg || "devDependencies" in oldPkg || "peerDependencies" in oldPkg;
             if (depsExists && !/\S+@\S+/.test(argPath)) {
               const {
                 devDependencies = {},
                 dependencies = {},
                 peerDependencies = {}
-              } = pkg;
+              } = oldPkg;
   
               const deps = Object.assign({}, devDependencies, peerDependencies, dependencies);
               const keys = Object.keys(deps);
@@ -100,138 +100,128 @@ await esbuild.build({
             let finalSubpath = subpath;
   
             // If the CDN supports package.json and some other npm stuff, it counts as an npm CDN
-            if (NPM_CDN) {
+            const ext = extname(subpath);
+            const isDir = ext.length === 0;
+            const dir = isDir ? subpath : "";
+
+            const pkgVariants = [
+              isDir ? {
+                path: `${parsed.name}@${parsed.version}${subpath}/package.json`,
+                isDir: true
+              } : null,
+              { path: `${parsed.name}@${parsed.version}/package.json` }
+            ].filter(x => x !== null);
+
+            let pkg: any = {};
+            let isDirPkgJSON = false;
+            const pkgVariantsLen = pkgVariants.length;
+            for (let i = 0; i < pkgVariantsLen; i++) {
+              const pkgMetadata = pkgVariants[i]!;
+              const { url } = getCDNUrl(pkgMetadata.path, origin);
+              const { href } = url;
+
               try {
-                const ext = extname(subpath);
-                const isDir = ext.length === 0;
-                const dir = isDir ? subpath : "";
-  
-                const pkgVariants = [
-                  isDir ? {
-                    path: `${parsed.name}@${parsed.version}${subpath}/package.json`,
-                    isDir: true
-                  } : null,
-                  { path: `${parsed.name}@${parsed.version}/package.json` }
-                ].filter(x => x !== null);
-  
-                let isDirPkgJSON = false;
-                const pkgVariantsLen = pkgVariants.length;
-                for (let i = 0; i < pkgVariantsLen; i++) {
-                  const pkgMetadata = pkgVariants[i]!;
-                  const { url } = getCDNUrl(pkgMetadata.path, origin);
-                  const { href } = url;
-  
+                if (FAILED_PKGJSON_URLs.has(href) && i < pkgVariantsLen - 1) {
+                  continue;
+                }
+
+                // Strongly cache package.json files
+                const res = await getRequest(url, true);
+                if (!res.ok) throw new Error(await res.text());
+
+                pkg = await res.json();
+                isDirPkgJSON = pkgMetadata.isDir ?? false;
+
+                const pkgPath = "/node_modules" + new URL(res.url).pathname;
+                if (!RESOLVED_URLs.has(url.href)) {
                   try {
-                    if (FAILED_PKGJSON_URLs.has(href) && i < pkgVariantsLen - 1) {
-                      continue;
-                    }
-  
-                    // Strongly cache package.json files
-                    const res = await getRequest(url, true);
-                    if (!res.ok) throw new Error(await res.text());
-  
-                    pkg = await res.json();
-                    isDirPkgJSON = pkgMetadata.isDir ?? false;
-  
-                    const pkgPath = "/node_modules" + new URL(res.url).pathname;
-                    if (!RESOLVED_URLs.has(url.href)) {
-                      try {
-                        // setFileSystem({
-                        //   [pkgPath]: JSON.stringify(pkg)
-                        // })
-                        FileSystem.set(pkgPath, JSON.stringify(pkg))
-                        RESOLVED_URLs.set(url.href, pkgPath)
-                      } catch (_e) { }
-                    }
-                    break;
-                  } catch (e) {
-                    FAILED_PKGJSON_URLs.add(href);
-  
-                    // If after checking all the different file extensions none of them are valid
-                    // Throw the first fetch error encountered, as that is generally the most accurate error
-                    if (i >= pkgVariantsLen - 1)
-                      throw e;
-                  }
+                    FileSystem.set(pkgPath, JSON.stringify(pkg))
+                    RESOLVED_URLs.set(url.href, pkgPath)
+                  } catch (_e) { }
                 }
-  
-                const relativePath = subpath.replace(/^\//, "./");
-  
-                let modernResolve: ReturnType<typeof resolve> | void;
-                let legacyResolve: ReturnType<typeof legacy> | void;
-  
-                let resolvedPath: string | void = subpath;
-  
-                try {
-                  // Resolving imports & exports from the package.json
-                  // If an import starts with "#" then it"s a subpath-import, and should be treated as so
-                  modernResolve = resolve(pkg, relativePath, { browser: true, conditions: ["module"] }) ||
-                    resolve(pkg, relativePath, { unsafe: true, conditions: ["deno", "worker", "production"] }) ||
-                    resolve(pkg, relativePath, { require: true });
-  
-                  if (modernResolve) {
-                    resolvedPath = Array.isArray(modernResolve) ? modernResolve[0] : modernResolve;
-                  }
-                  // deno-lint-ignore no-empty
-                } catch (e) { }
-  
-                if (!modernResolve) {
-                  // If the subpath has a package.json, and the modern resolve didn"t work for it
-                  // we can safely use legacy resolve, 
-                  // else, if the subpath doesn"t have a package.json, then the subpath is literal, 
-                  // and we should just use the subpath as it is
-                  if (isDirPkgJSON) {
-                    try {
-                      // Resolving using main, module, etc... from package.json
-                      legacyResolve = legacy(pkg, { browser: true }) ||
-                        legacy(pkg, { fields: ["unpkg", "bin"] });
-  
-                      if (legacyResolve) {
-                        // Some packages have `browser` fields in their package.json which have some values set to false
-                        // e.g. typescript - > https://unpkg.com/browse/typescript@4.9.5/package.json
-                        if (typeof legacyResolve === "object") {
-                          const values = Object.values(legacyResolve);
-                          const validValues = values.filter(x => x);
-                          if (validValues.length <= 0) {
-                            legacyResolve = legacy(pkg);
-                          }
-                        }
-  
-                        if (Array.isArray(legacyResolve)) {
-                          resolvedPath = legacyResolve[0];
-                        } else if (typeof legacyResolve === "object") {
-                          const legacyResults = legacyResolve;
-                          const allKeys = Object.keys(legacyResolve);
-                          const nonCJSKeys = allKeys.filter(key => {
-                            return !/\.cjs$/.exec(key) && !/src\//.exec(key) && legacyResults[key];
-                          });
-                          const keysToUse = nonCJSKeys.length > 0 ? nonCJSKeys : allKeys;
-                          resolvedPath = legacyResolve[keysToUse[0]] as string;
-                        } else {
-                          resolvedPath = legacyResolve;
-                        }
-                      }
-                    } catch (e) { }
-                  } else resolvedPath = relativePath;
-                }
-  
-                if (resolvedPath && typeof resolvedPath === "string") {
-                  finalSubpath = resolvedPath.replace(/^(\.\/)/, "/");
-                }
-  
-                if (dir && isDirPkgJSON) {
-                  finalSubpath = `${dir}${finalSubpath}`;
-                }
+                break;
               } catch (e) {
-                console.log([`You may want to change CDNs. The current CDN ${!/unpkg\.com/.test(origin) ? `"${origin}" doesn"t` : `path "${origin}${argPath}" may not`} support package.json files.\nThere is a chance the CDN you"re using doesn"t support looking through the package.json of packages. bundlejs will switch to inaccurate guesses for package versions. For package.json support you may wish to use https://unpkg.com or other CDN"s that support package.json.`], "warning");
-                console.warn(e);
+                FAILED_PKGJSON_URLs.add(href);
+
+                // If after checking all the different file extensions none of them are valid
+                // Throw the first fetch error encountered, as that is generally the most accurate error
+                if (i >= pkgVariantsLen - 1)
+                  throw e;
               }
+            }
+
+            const relativePath = subpath.replace(/^\//, "./");
+
+            let modernResolve: ReturnType<typeof resolve> | void;
+            let legacyResolve: ReturnType<typeof legacy> | void;
+
+            let resolvedPath: string | void = subpath;
+
+            try {
+              // Resolving imports & exports from the package.json
+              // If an import starts with "#" then it"s a subpath-import, and should be treated as so
+              modernResolve = resolve(pkg, relativePath, { browser: true, conditions: ["module"] }) ||
+                resolve(pkg, relativePath, { unsafe: true, conditions: ["deno", "worker", "production"] }) ||
+                resolve(pkg, relativePath, { require: true });
+
+              if (modernResolve) {
+                resolvedPath = Array.isArray(modernResolve) ? modernResolve[0] : modernResolve;
+              }
+              // deno-lint-ignore no-empty
+            } catch (e) { }
+
+            if (!modernResolve) {
+              // If the subpath has a package.json, and the modern resolve didn"t work for it
+              // we can safely use legacy resolve, 
+              // else, if the subpath doesn"t have a package.json, then the subpath is literal, 
+              // and we should just use the subpath as it is
+              if (isDirPkgJSON) {
+                try {
+                  // Resolving using main, module, etc... from package.json
+                  legacyResolve = legacy(pkg, { browser: true }) ||
+                    legacy(pkg, { fields: ["unpkg", "bin"] });
+
+                  if (legacyResolve) {
+                    // Some packages have `browser` fields in their package.json which have some values set to false
+                    // e.g. typescript - > https://unpkg.com/browse/typescript@4.9.5/package.json
+                    if (typeof legacyResolve === "object") {
+                      const values = Object.values(legacyResolve);
+                      const validValues = values.filter(x => x);
+                      if (validValues.length <= 0) {
+                        legacyResolve = legacy(pkg);
+                      }
+                    }
+
+                    if (Array.isArray(legacyResolve)) {
+                      resolvedPath = legacyResolve[0];
+                    } else if (typeof legacyResolve === "object") {
+                      const legacyResults = legacyResolve;
+                      const allKeys = Object.keys(legacyResolve);
+                      const nonCJSKeys = allKeys.filter(key => {
+                        return !/\.cjs$/.exec(key) && !/src\//.exec(key) && legacyResults[key];
+                      });
+                      const keysToUse = nonCJSKeys.length > 0 ? nonCJSKeys : allKeys;
+                      resolvedPath = legacyResolve[keysToUse[0]] as string;
+                    } else {
+                      resolvedPath = legacyResolve;
+                    }
+                  }
+                } catch (e) { }
+              } else resolvedPath = relativePath;
+            }
+
+            if (resolvedPath && typeof resolvedPath === "string") {
+              finalSubpath = resolvedPath.replace(/^(\.\/)/, "/");
+            }
+
+            if (dir && isDirPkgJSON) {
+              finalSubpath = `${dir}${finalSubpath}`;
             }
   
             // If the CDN is npm based then it should add the parsed version to the URL
             // e.g. https://unpkg.com/spring-easing@v1.0.0/
-            const version = NPM_CDN ? "@" + (pkg.version || parsed.version) : "";
-            const name = `${parsed.name}${version}${finalSubpath}`;
-            const { url } = getCDNUrl(name, origin);
+            const version = "@" + (pkg.version || parsed.version);
+            const { url } = getCDNUrl(`${parsed.name}${version}${finalSubpath}`, origin);
   
             const deps = Object.assign({}, oldPkg.devDependencies, oldPkg.dependencies, oldPkg.peerDependencies);
             const peerDeps = pkg.peerDependencies ?? {};
@@ -242,27 +232,21 @@ await esbuild.build({
   
             // Some typescript files don"t have file extensions but you can"t fetch a file without their file extension
             // so bundle tries to solve for that
-            let content: Uint8Array | undefined, urlStr: string, ext: string;
-            ({ content, url: urlStr, ext } = await determineExtension(url.toString(), false));
+            let content: Uint8Array | undefined, urlStr: string;
+            ({ content, url: urlStr } = await determineExtension(url.toString(), false));
   
-            const filePath = NPM_CDN ? "/node_modules" + new URL(urlStr).pathname : "/node_modules/" + name + ext;
-            if (content) {
-              try {
-                const priorArt = RESOLVED_URLs.has(argPath);
-                if (!priorArt) {
-                  // setFileSystem({
-                  //   [filePath]: decode(content)
-                  // })
-                  FileSystem.set(filePath, decoder.decode(content))
-                  RESOLVED_URLs.set(argPath, filePath)
-                }
-  
-                return { path: filePath, namespace: 'cdn' }
-              } catch (_e) { }
+            const filePath = "/node_modules" + new URL(urlStr).pathname;
+            FileSystem.set(filePath, decoder.decode(content))
+            RESOLVED_URLs.set(argPath, filePath)
+
+            return { 
+              path: filePath, 
+              namespace: 'cdn',
+              pluginData: { 
+                pkg: Object.assign({}, pkg, { peerDependencies: peerDeps }) 
+              },
             }
           } else if (args.path[0] === "." && /^\/node_modules/.test(args.importer)) {
-            const path = join(args.resolveDir, argPath);
-            if (RESOLVED_URLs.has(path)) return { path: RESOLVED_URLs.get(path), namespace: 'cdn' }
             const url = new URL(path.replace(/^\/node_modules/, ""), origin);
   
             // Some typescript files don"t have file extensions but you can"t fetch a file without their file extension
@@ -271,23 +255,11 @@ await esbuild.build({
             ({ content, url: urlStr } = await determineExtension(url.href, false));
   
             const filePath = "/node_modules" + new URL(urlStr).pathname;
-            if (content) {
-              try {
-                const priorArt = RESOLVED_URLs.has(path);
-                if (!priorArt) {
-                  // setFileSystem({
-                  //   [filePath]: decode(content)
-                  // })
-                  FileSystem.set(filePath, decoder.decode(content))
-                  RESOLVED_URLs.set(path, filePath)
-                } 
-  
-                return { path: filePath, namespace: 'cdn' }
-              } catch (_e) { }
-            }
+            FileSystem.set(filePath, decoder.decode(content))
+            RESOLVED_URLs.set(path, filePath)
+
+            return { path: filePath, namespace: 'cdn', pluginData: args.pluginData }
           }
-  
-          return undefined
         })
   
         // When a URL is loaded, we want to actually download the content
@@ -299,9 +271,9 @@ await esbuild.build({
               contents: FileSystem.get(args.path),
               loader: inferLoader(args.path),
               resolveDir: dirname(args.path),
+              pluginData: args.pluginData
             };
         });
-
       },
     }
   ]
--- utils.ts
+++ utils.ts
@@ -7,96 +7,17 @@ import { extname, isAbsolute, join } from 'node:path';
 export const DEFAULT_CDN_HOST = 'https://unpkg.com';
 
 /**
- * Returns the CDN style supported by certain CDN's
- * e.g. 
- * - `npm` - unpkg, skypack, esm.sh, and jsdelivr all support `npm` style imports for example they support adding versions to their URLs like this `https://unpkg.com/@okikio/animate@beta`
- * - `github` - github, and jsdelivr.gh both support `github` style imports, as in they don't support adding versions to their URLs like this `https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.min.js`
- * - `deno` - deno supports `deno` style imports, as in they don't support adding versions to their URLs like this `https://deno.land/x/brotli/mod.ts`
- * - `other` - CDNs that haven't been added to the list
- */
-export const getCDNStyle = (urlStr: string) => {
-    if (/^(skypack|esm|esm\.sh|unpkg|jsdelivr|esm\.run)\:?/.test(urlStr) || /^https?:\/\/(cdn\.skypack\.dev|cdn\.esm\.sh|cdn\.jsdelivr\.net\/npm|unpkg\.com)/.test(urlStr)) 
-        return "npm";
-
-    else if (/^(jsdelivr\.gh|github)\:?/.test(urlStr) || /^https?:\/\/(cdn\.jsdelivr\.net\/gh|raw\.githubusercontent\.com)/.test(urlStr)) 
-        return "github";
-
-    else if (/^(deno)\:?/.test(urlStr) || /^https?:\/\/(deno\.land\/x)/.test(urlStr) )
-        return "deno";
-
-    return "other";
-}
-
-/**
- * Based on the URL scheme of the import, this method will return an actual CDN host origin to use,
- * e.g. 
- * ```ts
- * getCDNHost("react") //= https://unpkg.com
- * getCDNHost("react", "https://cdn.skypack.dev") //= https://cdn.skypack.dev/
- * 
- * // CDN URL Schemes take precedence above everything
- * getCDNHost("esm:react", "https://cdn.skypack.dev") //= https://cdn.esm.sh/
- * ```
- * 
- * > _**Note**: The returned CDN URL string will end with a '/' e.g. `https://cdn.esm.sh/`_
- * 
- * @param importStr imports to find a CDN for
- * @param cdn The default CDN host to use. This can change based on the config of the user. This may be diregarded if the `importStr` has a CDN URL Scheme
- * @returns CDN URL host string
- */
-export const getCDNOrigin = (importStr: string, cdn = DEFAULT_CDN_HOST) => {
-    // `skypack:` --> `https://cdn.skypack.dev`
-    if (/^skypack\:/.test(importStr))
-        cdn = `https://cdn.skypack.dev`;
-
-    // `esm.sh:` or `esm:` --> `https://cdn.esm.sh`
-    else if (/^(esm\.sh|esm)\:/.test(importStr))
-        cdn = `https://cdn.esm.sh`;
-
-    // `unpkg:` --> `https://unpkg.com`
-    else if (/^unpkg\:/.test(importStr))
-        cdn = `https://unpkg.com`;
-
-    // (NPM) `jsdelivr:` or `esm.run:` --> `https://cdn.jsdelivr.net/npm`
-    else if (/^(jsdelivr|esm\.run)\:/.test(importStr))
-        cdn = `https://cdn.jsdelivr.net/npm`;
-
-    // (GitHub) `jsdelivr.gh:` --> `https://cdn.jsdelivr.net/gh`
-    else if (/^(jsdelivr\.gh)\:/.test(importStr))
-        cdn = `https://cdn.jsdelivr.net/gh`;
-
-    // `deno:` --> `https://deno.land/x`
-    else if (/^(deno)\:/.test(importStr))
-        cdn = `https://deno.land/x`;
-
-    // `github:` --> `https://raw.githubusercontent.com`
-    else if (/^(github)\:/.test(importStr)) 
-        cdn = `https://raw.githubusercontent.com`;
-
-    return /\/$/.test(cdn) ? cdn : `${cdn}/`;
-}
-
-/**
- * Remove CDN URL Schemes like `deno:...`, `unpkg:...`, etc... and known CDN hosts, e.g. `https://raw.githubusercontent.com/...`, `https://cdn.skypack.dev/...`, etc...  Leaving only the import path
- */
- export const getPureImportPath = (importStr: string) => 
-    importStr
-        .replace(/^(skypack|esm|esm\.sh|unpkg|jsdelivr|jsdelivr\.gh|esm\.run|deno|github)\:/, "")
-        .replace(/^https?:\/\/(cdn\.skypack\.dev|cdn\.esm\.sh|cdn\.jsdelivr\.net\/npm|unpkg\.com|cdn\.jsdelivr\.net\/gh|raw\.githubusercontent\.com|deno\.land\/x)/, "")
-        .replace(/^\//, "");
-
-/**
  * Generates a CDN URL for an import, taking advantage of CDN URL Schemes in imports and the default CDN hosts parameter to decide the CDN host
  * Read through {@link getCDNOrigin} and {@link getPureImportPath}
  */
 export const getCDNUrl = (importStr: string, cdn = DEFAULT_CDN_HOST) => {
-    let origin = getCDNOrigin(importStr, cdn);
-    let path = getPureImportPath(importStr);
-    let url = new URL(path, origin);
-    return { import: importStr, path, origin, cdn, url };
+	let origin = /\/$/.test(cdn) ? cdn : `${cdn}/`;
+	let path = importStr.replace(/^\//, "");
+	let url = new URL(path, origin);
+	return { import: importStr, path, origin, cdn, url };
 }
 
-/**  */ 
+/**  */
 export const RESOLVE_EXTENSIONS = [".tsx", ".ts", ".jsx", ".js", ".css", ".json"];
 
 /**
@@ -104,38 +25,38 @@ export const RESOLVE_EXTENSIONS = [".tsx", ".ts", ".jsx", ".js", ".css", ".json"
  * Uses the file extention to determine the loader esbuild should use
  */
 export const inferLoader = (urlStr: string): Loader => {
-    const ext = extname(urlStr);
-    if (RESOLVE_EXTENSIONS.includes(ext))
-        // Resolve all .js and .jsx files to .ts and .tsx files
-        return (/\.js(x)?$/.test(ext) ? ext.replace(/^\.js/, ".ts") : ext).slice(1) as Loader;
+	const ext = extname(urlStr);
+	if (RESOLVE_EXTENSIONS.includes(ext))
+		// Resolve all .js and .jsx files to .ts and .tsx files
+		return (/\.js(x)?$/.test(ext) ? ext.replace(/^\.js/, ".ts") : ext).slice(1) as Loader;
 
-    if (ext === ".mjs" || ext === ".cjs") return "ts"; // "js"
-    if (ext === ".mts" || ext === ".cts") return "ts";
-    
-    if (ext == ".scss") return "css";
+	if (ext === ".mjs" || ext === ".cjs") return "ts"; // "js"
+	if (ext === ".mts" || ext === ".cts") return "ts";
 
-    if (ext == ".png" || ext == ".jpeg" || ext == ".ttf") return "dataurl";
-    if (ext == ".svg" || ext == ".html" || ext == ".txt") return "text";
-    if (ext == ".wasm") return "file";
+	if (ext == ".scss") return "css";
 
-    return ext.length ? "text" : "ts";
+	if (ext == ".png" || ext == ".jpeg" || ext == ".ttf") return "dataurl";
+	if (ext == ".svg" || ext == ".html" || ext == ".txt") return "text";
+	if (ext == ".wasm") return "file";
+
+	return ext.length ? "text" : "ts";
 }
 
 
 const WHITESPACE_ENCODINGS: Record<string, string> = {
-    "\u0009": "%09",
-    "\u000A": "%0A",
-    "\u000B": "%0B",
-    "\u000C": "%0C",
-    "\u000D": "%0D",
-    "\u0020": "%20",
-  };
-  
-  export function encodeWhitespace(string: string): string {
-    return string.replaceAll(/[\s]/g, (c) => {
-      return WHITESPACE_ENCODINGS[c] ?? c;
-    });
-  }
+	"\u0009": "%09",
+	"\u000A": "%0A",
+	"\u000B": "%0B",
+	"\u000C": "%0C",
+	"\u000D": "%0D",
+	"\u0020": "%20",
+};
+
+export function encodeWhitespace(string: string): string {
+	return string.replaceAll(/[\s]/g, (c) => {
+		return WHITESPACE_ENCODINGS[c] ?? c;
+	});
+}
 
 /** 
  * Based on https://github.com/egoist/play-esbuild/blob/main/src/lib/path.ts#L123
@@ -143,16 +64,16 @@ const WHITESPACE_ENCODINGS: Record<string, string> = {
  * Support joining paths to a URL
  */
 export const urlJoin = (urlStr: string, ...args: string[]) => {
-    const url = new URL(urlStr);
-    url.pathname = encodeWhitespace(
-        join(url.pathname, ...args).replace(/%/g, "%25").replace(/\\/g, "%5C"),
-    );
-    return url.toString();
+	const url = new URL(urlStr);
+	url.pathname = encodeWhitespace(
+		join(url.pathname, ...args).replace(/%/g, "%25").replace(/\\/g, "%5C"),
+	);
+	return url.toString();
 }
 
 /**
  * An import counts as a bare import if it's neither a relative import of an absolute import
  */
- export const isBareImport = (importStr: string) => {
-    return /^(?!\.).*/.test(importStr) && !isAbsolute(importStr);
+export const isBareImport = (importStr: string) => {
+	return /^(?!\.).*/.test(importStr) && !isAbsolute(importStr);
 }
